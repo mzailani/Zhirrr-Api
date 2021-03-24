ffde307a974a194f00e568532961d4eee26466f3
mzailani <74986677+mzailani@users.noreply.github.com>
2021-03-24 16:07:26 +0700

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,314 @@
+__path = process.cwd()
+
+var express = require('express');
+var db = require(__path + '/database/db');
+try {
+var zahirr = db.get("zahirr");
+} catch (e) {
+	console.log('')
+}
+
+var creatorList = ['@mzailani8'];
+var creator = creatorList[Math.floor(Math.random() * creatorList.length)];
+
+
+var ytdl = require('ytdl-core');
+var ytpl = require('ytpl');
+var secure = require('ssl-express-www');
+var cors = require('cors');
+var scrapeYt = require("scrape-yt");
+var fetch = require('node-fetch');
+var cheerio = require('cheerio');
+var request = require('request');
+var TikTokScraper = require('tiktok-scraper');
+var router  = express.Router();
+
+var { color, bgcolor } = require(__path + '/lib/color.js');
+var { fetchJson } = require(__path + '/lib/fetcher.js')
+var options = require(__path + '/lib/options.js');
+var {
+	Nulis,
+	Vokal,
+	Base,
+	Searchnabi,
+    Gempa
+} = require('./../lib');
+var cookie = "HSID=A7EDzLn3kae2B1Njb;SSID=AheuwUjMojTWvA5GN;APISID=cgfXh13rQbb4zbLP/AlvlPJ2xBJBsykmS_;SAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;__Secure-3PAPISID=m82rJG4AC9nxQ5uG/A1FotfA_gi9pvo91C;VISITOR_INFO1_LIVE=RgZLnZtCoPU;LOGIN_INFO=AFmmF2swRQIhAOXIXsKVou2azuz-kTsCKpbM9szRExAMUD-OwHYiuB6eAiAyPm4Ag3O9rbma7umBK-AG1zoGqyJinh4ia03csp5Nkw:QUQ3MjNmeXJ0UHFRS3dzaTNGRmlWR2FfMDRxa2NRYTFiN3lfTEdOVTc4QUlwbUI4S2dlVngxSG10N3ZqcHZwTHBKano5SkN2dDlPSkhRMUtReE42TkhYeUVWS3kyUE1jY2I1QzA1MDZBaktwd1llWU9lOWE4NWhoZV92aDkxeE9vMTNlcG1uMU9rYjhOaDZWdno2ZzN3TXl5TVNhSjNBRnJaMExrQXpoa2xzRVUteFNWZDI5S0Fn;PREF=app=desktop&f4=4000000&al=id;SID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1njBpLTOpxSfN-EaYCRSiDg.;YSC=HCowA1fmvzo;__Secure-3PSID=2wezCMTUkWN3YS1VmS_DXaEU84J0pZIQdemM8Zry-uzWm8y1dajgWzlBh9TgKapGOwuXfA.;SIDCC=AJi4QfFK0ri9fSfMjMQ4tOJNp6vOb9emETXB_nf2S05mvr2jBlmeEvlSsQSzPMuJl_V0wcbL1r8;__Secure-3PSIDCC=AJi4QfGeWHx-c4uTpU1rXCciO1p0s2fJWU07KrkZhWyD1Tqi8LyR-kHuBwHY9mViVYu1fRh2PA";
+
+loghandler = {
+    notparam: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter apikey',
+        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/628881277513'
+    },
+    notkey: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter key'
+    },
+    noturl: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter url'
+    },
+    notkata: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter kata'
+    },
+    nottext: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text'
+    },
+    nottext2: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text2'
+    },
+    notnabi: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter nabi'
+    },
+    nottext3: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter text3'
+    },
+    nottheme: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter theme'
+    },
+    notusername: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter username'
+    },
+    notvalue: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter value'
+    },
+    notheme: {
+    	status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'theme tidak tersedia silahkan masukkan texmaker/list atau baca documentasi'
+     },
+    invalidKey: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'apikey invalid, gak punya apikey? chat gw aja yaaa di wa.me/628881277513'
+    },
+    invalidlink: {
+        status: false,
+        creator: `${creator}`,
+        message: 'error, mungkin link anda tidak valid.'
+    },
+    invalidkata: {
+        status: false,
+        creator: `${creator}`,
+        message: 'error, mungkin kata tidak ada dalam api.'
+    },
+    notAddApiKey: {
+        status: false,
+        creator: `${creator}`,
+        code: 406,
+        message: 'masukan parameter status, apikeyInput, email, nomorhp, name, age, country, exp'
+    },
+    error: {
+        status: false,
+        creator: `${creator}`,
+        message: 'mungkin sedang dilakukan perbaikan'
+    }
+}
+
+var len = 15
+        var arr = '123456789abcdefghijklmnopqrstuvwxyz'
+        var random = '';
+
+        for (var i = len; i > 0; i--) {
+            random += arr[Math.floor(Math.random() * arr.length)];
+        }
+
+        var lenn = 5
+        var randomlagi = '';
+
+        for (var i = lenn; i > 0; i--) {
+            randomlagi += arr[Math.floor(Math.random() * arr.length)];
+        }
+
+        var randomTextNumber = random+randomlagi+'---------MzailaniGanteng'+'ZAILANI--GANS';
+        
+ 
+ async function cekApiKey(api) {
+ 	ap = await zahirr.findOne({apikey:api})
+ return ap;
+ }
+router.get('/find', async (req, res, next) => {
+    var apikey = req.query.apikey
+    if (!apikey) return res.json(loghandler.notparam)
+    if (apikey != 'ZailaniGans') return res.json(loghandler.invalidKey)
+
+    try {
+        zahirr.find()
+            .then(result => {
+                res.json({
+                    status: true,
+                    creator: `${creator}`,
+                    result
+                })
+        })
+    } catch (e) {
+        console.log(e)
+        res.json(loghandler.error)
+    }
+})
+
+router.get('/cekapikey', async (req, res, next) => {
+	var apikeyInput = req.query.apikey
+	if(!apikeyInput) return res.json(loghandler.notparam)
+	a = await cekApiKey(apikeyInput)
+	if (a) {
+	json = JSON.stringify({
+		status: true,
+		creator: creator,
+		result: {
+            status:a.status,
+			id: a._id,
+			apikey: a.apikey,
+			more_info: {
+				email: a.email,
+				nomor_hp: a.nomor_hp,
+				name: a.name,
+				age: a.age,
+				country: a.country,
+				exp:a.exp,
+			},
+		},
+		message: `jangan lupa follow ${creator}`
+	})
+} else {
+	json = JSON.stringify({
+		status: false
+	})
+}
+res.send(JSON.parse(json))
+})
+
+router.get('/addapikey', (req, res, next) => {
+    var apikey = req.query.apikey,
+        status = req.query.status,
+        apikeyInput  = req.query.apikeyInput,
+        email = req.query.email,
+        nomorhp = req.query.nomorhp
+        name = req.query.name,
+        age = req.query.age,
+        country = req.query.country;
+        exp = req.query.exp;
+
+    if (!apikey) return res.json(loghandler.notparam)
+    if (!(status && apikeyInput && email && nomorhp && name && age && country && exp)) return res.json(loghandler.notAddApiKey)
+    if (apikey != 'ZailaniGans') return res.json(loghandler.invalidKey)
+
+    try {
+        zahirr.insert({
+        	status: status,
+            apikey: apikeyInput,
+            email: email,
+            nomor_hp: nomorhp,
+            name: name,
+            age: age,
+            country: country,
+            exp: exp
+        })
+        .then(() => {
+              res.json({
+                  status: true,
+                  creator: `${creator}`,
+                  result: 'berhasil menambah data, status : ' + status + ', apikey : ' + apikeyInput + ', email : ' + email + ', nomor_hp : ' + nomorhp + ', name :  ' + name + ', age : ' + age + ', country : ' + country + ', exp : ' + exp
+              })
+        })
+    } catch (e) {
+        console.log(e)
+        res.json(loghandler.error)
+    }
+})
+
+router.get('/remove', (req, res, next) => {
+    var apikey = req.query.apikey,
+        status = req.query.status,
+        apikeyInput  = req.query.apikeyInput,
+        email = req.query.email,
+        nomorhp = req.query.nomorhp
+        name = req.query.name,
+        age = req.query.age,
+        country = req.query.country;
+        exp = req.query.exp;
+
+    if (!apikey) return res.json(loghandler.notparam)
+    if (!(status && apikeyInput && email && nomorhp && name && age && country && exp)) return res.json(loghandler.notAddApiKey)
+    if (apikey != 'ZailaniGans') return res.json(loghandler.invalidKey)
+
+    try {
+        zahirr.remove({
+            status: status,
+            apikey: apikeyInput,
+            email: email,
+            nomor_hp: nomorhp,
+            name: name,
+            age: age,
+            country: country,
+            exp: exp
+        })
+        .then(() => {
+             res.json({
+                  status: true,
+                  creator: `${creator}`,
+                  result: 'berhasil menghapus data, status : ' + status + ', apikey : ' + apikeyInput + ', email : ' + email + ', nomor_hp : ' + nomorhp + ', name :  ' + name + ', age : ' + age + ', country : ' + country + ', exp : ' + exp
+              })
+        })
+    } catch (e) {
+        console.log(e)
+        res.json(loghandler.error)
+    }
+})
+
+
+router.get('/nsfw/nsfwNeko', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+	if(apikeyInput != 'ZailaniGans') return res.json(loghandler.invalidKey)
+
+       fetch(encodeURI(`https://raw.githubusercontent.com/alpin1234567/Js-Project/master/jsinnsfw/nsfwNeko.json`))
+        .then(response => response.json())
+        .then(data => {
+        var isi = data[Math.floor(Math.random() * data.length)];
+        var result = isi;
+             res.json({
+                 result
+             })
+         })
+         .catch(e => {
+         	res.json(loghandler.error)
+})
+})
+
+
+module.exports = router
